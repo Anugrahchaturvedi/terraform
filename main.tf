3c0bdd4c276a3162a02970a52457b166d2b54f05
Anugrah Chaturvedi <73109944+Anugrahchaturvedi@users.noreply.github.com>
2022-02-03 23:35:51 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "Terra"
+  location = "Central India"
+}
+
+# vnet
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terra-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    name = "Terra"
+  }
+}
+# subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# network interface
+resource "azurerm_network_interface" "ni" {
+  name                = "ni-card"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "terra"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "anugrah"
+  network_interface_ids = [
+    azurerm_network_interface.ni.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  os_profile{
+    computer_name = "hostname"
+    admin_username = "anugrah"
+    admin_password = "anugrah@123"
+  }
+
+}
