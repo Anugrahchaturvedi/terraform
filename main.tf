50f9f2d81f7be21d726aaa4daaa919a0aacb2fcd
Anugrah Chaturvedi <73109944+Anugrahchaturvedi@users.noreply.github.com>
2022-02-10 23:21:20 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,75 +1,31 @@
+#terraform setting block (version & all Providers)
 terraform {
-  required_version = ">=0.12"
-
+  required_version = ">=0.13"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~> 2.65"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.15.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
     }
   }
-}
-resource "azurerm_resource_group" "rg" {
-  name     = "Terra"
-  location = "Central India"
-}
+  //this is Storage for state file
+  backend "azurerm" {
+    resource_group_name  = "terraform-storage-rg" // rg-name of storage
+    storage_account_name = "kushalkush867"        //r-name of storage
+    container_name       = "tf-state-file"        //container-name
+    key                  = "terraform.tfstate"    //local filename
 
-# vnet
-resource "azurerm_virtual_network" "vnet" {
-  name                = "terra-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags = {
-    name = "Terra"
-  }
-}
-# subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# network interface
-resource "azurerm_network_interface" "ni" {
-  name                = "ni-card"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
   }
 }
 
-# Virtual machine
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "terra"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
-  admin_username      = "anugrah"
-  network_interface_ids = [
-    azurerm_network_interface.ni.id,
-  ]
-
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  os_profile{
-    computer_name = "hostname"
-    admin_username = "anugrah"
-    admin_password = "anugrah@123"
-  }
-
+provider "azurerm" {
+  features {}
 }
+
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "location" {
+  value = azurerm_resource_group.aks.location
+
+}
+ output "resource_group_id" {
+   value = azurerm_resource_group.aks.id
+ }
